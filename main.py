740ffaae8b92e480d4e732fda5120806e887a9d9
AbrahamGonMo <87501382+AbrahamGonMo@users.noreply.github.com>
2021-07-25 16:53:22 -0500

    Cambio de Nombre

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+import time
+
+def numero():
+    return random.randint(1,100)
+
+def saludo():
+    msj = "Hola este programa te devolvera tu numero de la suerte del dia de hoy!!"
+    return msj
+
+if __name__ == "__main__":
+    print()
+    print(saludo())
+    print("\nTu numero de la suerte es el...")
+    time.sleep(3)
+    print()
+    print("\t---> ",numero())
+
+
